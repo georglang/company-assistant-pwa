0b32e94b7d737efbf33fe80463fd3d377ba4e78f
Georg Lang <mail@georglang.com>
2021-01-24 19:18:53 +0100

    feat: sign-in component

---
+++ projects/company-assistant-pwa/src/app/core/auth/sign-in/sign-in.component.ts
@@ -0,0 +1,47 @@
+import { Input, Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit {
+  @Input() error: string | null;
+  @Output() submitEM = new EventEmitter();
+
+  signInForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  hide = true;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {}
+
+  get emailInput() {
+    return this.signInForm.get('email');
+  }
+
+  get passwordInput() {
+    return this.signInForm.get('password');
+  }
+
+  submit() {
+    if (this.signInForm.valid) {
+      this.submitEM.emit(this.signInForm.value);
+      this.authService
+        .login(
+          this.signInForm.get('email').value,
+          this.signInForm.get('password').value
+        )
+        .then((data) => {
+          this.router.navigate(['working-hours']);
+        });
+    }
+  }
+}

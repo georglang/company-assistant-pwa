4db981a74b78735dfc6ea93cd3193eed708549e9
Georg Lang <mail@georglang.com>
2021-01-24 19:36:03 +0100

    feat(app.module): include firebase modules and components

--- projects/company-assistant-pwa/src/app/app.module.ts
+++ projects/company-assistant-pwa/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 /* NgRx*/
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
-import { OAuthModule } from 'angular-oauth2-oidc';
+import { EffectsModule } from '@ngrx/effects';
 
 import { environment } from '../environments/environment.prod';
 import { AppRoutingModule } from './app.routing.module';
@@ -17,12 +21,22 @@ import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './features/home/home.component';
 
 import { reducers, metaReducers } from './state/app.state';
-import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './core/auth/state/auth.effects';
-import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { firebaseConfig } from 'firebaseConfig';
+import { AuthService } from './core/auth/auth.service';
+import { SignInComponent } from './core/auth/sign-in/sign-in.component';
+import { SignUpComponent } from './core/auth/sign-up/sign-up.component';
+import { ForgotPasswortComponent } from './core/auth/forgot-passwort/forgot-passwort.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswortComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -30,14 +44,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     CoreModule,
     SharedModule,
     FlexLayoutModule,
-
-    // OAuth
-    OAuthModule.forRoot({
-      resourceServer: {
-        allowedUrls: [''],
-        sendAccessToken: true
-      }
-    }),
+    FormsModule,
+    ReactiveFormsModule,
+    // Auth
+    AngularFireModule.initializeApp(firebaseConfig.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    // NgRx
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument({
@@ -45,11 +58,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       maxAge: 25,
       logOnly: environment.production
     }),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
   ],
 
   //ngrx
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

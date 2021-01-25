631ec19939eef2ed29a108152d7390aa1bc20fdc
Georg Lang <mail@georglang.com>
2021-01-25 06:51:12 +0100

    style(forgot-password): responsive styles

--- projects/company-assistant-pwa/src/app/core/auth/forgot-password/forgot-password.component.spec.ts
+++ projects/company-assistant-pwa/src/app/core/auth/forgot-password/forgot-password.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { ForgotPasswortComponent } from './forgot-passwort.component';
+import { ForgotpasswordComponent } from './forgot-password.component';
 
-describe('ForgotPasswortComponent', () => {
-  let component: ForgotPasswortComponent;
-  let fixture: ComponentFixture<ForgotPasswortComponent>;
+describe('ForgotpasswordComponent', () => {
+  let component: ForgotpasswordComponent;
+  let fixture: ComponentFixture<ForgotpasswordComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ForgotPasswortComponent ]
+      declarations: [ ForgotpasswordComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ForgotPasswortComponent);
+    fixture = TestBed.createComponent(ForgotpasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- projects/company-assistant-pwa/src/app/core/auth/forgot-password/forgot-password.component.ts
+++ projects/company-assistant-pwa/src/app/core/auth/forgot-password/forgot-password.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-forgot-passwort',
-  templateUrl: './forgot-passwort.component.html',
-  styleUrls: ['./forgot-passwort.component.scss']
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswortComponent implements OnInit {
+export class ForgotpasswordComponent {
   @Input() error: string | null;
 
-  forgotPasswortForm: FormGroup = new FormGroup({
+  forgotpasswordForm: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required])
   });
   constructor(private authService: AuthService) {}
-
-  ngOnInit() {}
 
   submit(passwordResetEmail: string): void {
     this.authService.forgotPassword(passwordResetEmail);
   }
 
   get emailInput() {
-    return this.forgotPasswortForm.get('email');
+    return this.forgotpasswordForm.get('email');
   }
 }

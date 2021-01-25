e2ce950522eb391f97f1b8f65a74ab482ff66c65
Georg Lang <mail@georglang.com>
2021-01-25 06:53:38 +0100

    feat(local-storage): get stored item

--- projects/company-assistant-pwa/src/app/core/services/local-storage/local-storage.service.ts
+++ projects/company-assistant-pwa/src/app/core/services/local-storage/local-storage.service.ts
@@ -37,8 +37,12 @@ export class LocalStorageService {
     }, {});
   }
 
-  setItem(key: string, value: ILocalStorageValue): void {
+  setItem(key: string, value: any): void {
     localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getItem(key: string) {
+    return localStorage.getItem('user');
   }
 
   removeItem(key: string): void {

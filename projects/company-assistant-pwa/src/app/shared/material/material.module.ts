95db94c527e583b027c39a8bdb509701619e6913
Georg Lang <mail@georglang.com>
2021-01-30 09:15:46 +0100

    feat(material.module): add material modules

--- projects/company-assistant-pwa/src/app/shared/material/material.module.ts
+++ projects/company-assistant-pwa/src/app/shared/material/material.module.ts
@@ -1,48 +1,68 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+// Angular Material Modules
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatListModule,
-    MatMenuModule,
-    MatIconModule,
-    MatSelectModule,
-    MatTooltipModule,
-    MatSnackBarModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatIconModule,
+    MatDatepickerModule,
     MatInputModule,
-    MatCardModule
+    MatNativeDateModule,
+    MatListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatAutocompleteModule,
+    MatSidenavModule
   ],
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
-    MatListModule,
-    MatMenuModule,
-    MatIconModule,
-    MatSelectModule,
-    MatTooltipModule,
-    MatSnackBarModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatIconModule,
+    MatDatepickerModule,
     MatInputModule,
-    MatCardModule
+    MatNativeDateModule,
+    MatListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatAutocompleteModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule {}

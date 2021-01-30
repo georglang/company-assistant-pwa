d916bdf466ffea6258c27f314ba89bfb2ffee873
Georg Lang <mail@georglang.com>
2021-01-30 09:06:16 +0100

    feat(order.module): import modules and components

--- projects/company-assistant-pwa/src/app/features/order/order.module.ts
+++ projects/company-assistant-pwa/src/app/features/order/order.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderRoutingModule } from './order.routing';
 import { SharedModule } from '../../shared/shared.module';
-import { OrderListComponent } from './orderList/OrderList.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { SettingsDialogComponent } from '../../shared/components/settings-dialog/settings-dialog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ConfirmDeleteDialogComponent } from '../../shared/components/confirm-delete-dialog/confirm-delete-dialog.component';
+import { MessageService } from '../../shared/services/message-service/message.service';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, OrderRoutingModule],
-  declarations: [OrderListComponent]
+  imports: [
+    CommonModule,
+    SharedModule,
+    OrderRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    OrderListComponent,
+    SettingsDialogComponent,
+    ConfirmDeleteDialogComponent
+  ],
+  providers: [MessageService]
 })
 export class OrderModule {}

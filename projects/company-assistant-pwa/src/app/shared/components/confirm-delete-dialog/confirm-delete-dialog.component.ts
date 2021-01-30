25d233e285963d572427d6d198bbc4f4b1205be0
Georg Lang <mail@georglang.com>
2021-01-30 09:16:14 +0100

    feat(confirm-delete-dialog)

---
+++ projects/company-assistant-pwa/src/app/shared/components/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-delete-dialog',
+  templateUrl: './confirm-delete-dialog.component.html',
+  styleUrls: ['./confirm-delete-dialog.component.sass']
+})
+export class ConfirmDeleteDialogComponent implements OnInit {
+  public modalTitle: string;
+  public passwordCorrect: string;
+  public hide = true;
+  public isPasswordCorrect = false;
+
+  public passwordForm: FormGroup = new FormGroup({
+    password: new FormControl('', [Validators.required, Validators.min(3)])
+  });
+
+  get passwordInput() {
+    return this.passwordForm.get('password');
+  }
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit() {
+    this.passwordForm.controls['password'].valueChanges.subscribe((input) => {
+      if (input === 'Löwenbräu') {
+        this.isPasswordCorrect = true;
+      }
+    });
+  }
+}

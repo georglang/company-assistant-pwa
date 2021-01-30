2f3de519f6ed0d4a82593e0b83aa4aed31c9e8e7
Georg Lang <mail@georglang.com>
2021-01-30 09:15:20 +0100

    feat(shared.module): add header component

--- projects/company-assistant-pwa/src/app/shared/shared.module.ts
+++ projects/company-assistant-pwa/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule],
-  declarations: [],
-  exports: [MaterialModule]
+  declarations: [HeaderComponent],
+  exports: [MaterialModule, HeaderComponent]
 })
 export class SharedModule {}

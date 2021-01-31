1089e35668823e96aee8f3a34b66b84b71af5a89
Georg Lang <mail@georglang.com>
2021-01-31 16:20:47 +0100

    feat(order.routing): order-details and edit-order

--- projects/company-assistant-pwa/src/app/features/order/order.routing.ts
+++ projects/company-assistant-pwa/src/app/features/order/order.routing.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditOrderComponent } from './edit-order/edit-order.component';
 import { OrderListComponent } from './order-list/order-list.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: OrderListComponent
+  },
+  {
+    path: 'order-details',
+    component: OrderDetailComponent
+  },
+  {
+    path: 'edit-order',
+    component: EditOrderComponent
   }
 ];
 

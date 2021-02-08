19f2e2d82e9a18cdec6f0383f8b67a608787cfb8
Georg Lang <mail@georglang.com>
2021-02-08 10:28:15 +0100

    feat(order): make order class id and workingHour properties optional

--- projects/company-assistant-pwa/src/app/features/order/Order.ts
+++ projects/company-assistant-pwa/src/app/features/order/Order.ts
@@ -1,11 +1,13 @@
 import { Timestamp } from '@firebase/firestore-types';
+import { IWorkingHour } from '../working-hour/services/firestore-working-hour-service/WorkingHour';
 
 export interface IOrder {
   companyName: string;
   contactPerson: string;
   date: Timestamp;
-  id: any;
   location: string;
+  id?: any;
+  workingHours?: IWorkingHour[];
 }
 
 export interface IFlattenOrder {
@@ -18,20 +20,23 @@ export class Order implements IOrder {
   public companyName: string;
   public contactPerson: string;
   public date: Timestamp;
-  public id: any;
   public location: string;
+  public id?: any;
+  public workingHours?: IWorkingHour[];
 
   constructor(
     date: Timestamp,
     companyName: string,
     contactPerson: string,
-    id: any,
-    location: string
+    location: string,
+    id?: any,
+    workingHours?: IWorkingHour[]
   ) {
     this.companyName = companyName;
     this.contactPerson = contactPerson;
     this.date = date;
     this.id = id;
     this.location = location;
+    this.workingHours = workingHours;
   }
 }

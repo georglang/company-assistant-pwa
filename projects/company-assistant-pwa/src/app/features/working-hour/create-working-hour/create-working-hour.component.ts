a6b882275bb9bab96a0dc8b242bd29b468f07036
Georg Lang <mail@georglang.com>
2021-02-14 12:12:08 +0100

    feat(working-hour): create-working-hour component

---
+++ projects/company-assistant-pwa/src/app/features/working-hour/create-working-hour/create-working-hour.component.ts
@@ -0,0 +1,162 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
+import { WorkingHour } from '../WorkingHour';
+import { FirestoreWorkingHourService } from '../services/firestore-working-hour-service/firestore-working-hour.service';
+import { MessageService } from '../../../shared/services/message-service/message.service';
+
+@Component({
+  selector: 'app-create-working-hour',
+  templateUrl: './create-working-hour.component.html',
+  styleUrls: ['./create-working-hour.component.scss']
+})
+export class CreateWorkingHourComponent implements OnInit {
+  public createWorkingHourForm: FormGroup;
+  private routeParamOrderId;
+  public submitted = false;
+  public employees = [
+    {
+      value: 'Erle Wastl',
+      viewValue: 'Erle Wastl'
+    },
+    {
+      value: 'Greisl Martin',
+      viewValue: 'Greisl Martin'
+    },
+    {
+      value: 'Martin Johann',
+      viewValue: 'Martin Johann'
+    },
+    {
+      value: 'Speer Leonhard',
+      viewValue: 'Speer Leonhard'
+    },
+    {
+      value: 'Strobel Michael',
+      viewValue: 'Strobel Michael'
+    },
+    {
+      value: 'Tschabi Matthias',
+      viewValue: 'Tschabi Matthias'
+    }
+  ];
+  public tools = [
+    {
+      value: 'Mann',
+      viewValue: 'Mann'
+    },
+    {
+      value: 'Mann mit Motorsäge',
+      viewValue: 'Mann mit Motorsäge'
+    },
+    {
+      value: 'Seilschlepper',
+      viewValue: 'Seilschlepper'
+    },
+    {
+      value: 'Schlepper + Rückewagen',
+      viewValue: 'Schlepper + Rückewagen'
+    },
+    {
+      value: 'HSM',
+      viewValue: 'HSM'
+    },
+    {
+      value: 'HSM + Woody',
+      viewValue: 'HSM + Woody'
+    },
+    {
+      value: 'Rückezug',
+      viewValue: 'Rückezug'
+    },
+    {
+      value: 'Rückezug + Winde + Bänder',
+      viewValue: 'Rückezug + Winde + Bänder'
+    },
+    {
+      value: 'Harvester',
+      viewValue: 'Harvester'
+    }
+  ];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private firestoreWorkingHourService: FirestoreWorkingHourService,
+    private messageService: MessageService,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    this.createWorkingHourForm = this.formBuilder.group({
+      date: ['', Validators.required],
+      description: ['', Validators.required],
+      employee: ['', Validators.required],
+      workingHours: ['', Validators.required]
+    });
+
+    this.route.params.subscribe((params) => {
+      this.routeParamOrderId = params.id;
+    });
+  }
+
+  public navigateToOrderList() {
+    this.location.back();
+  }
+
+  public createWorkingHour(formInput: any, orderId: string): void {
+    const workingHour = new WorkingHour(
+      formInput.date,
+      formInput.description,
+      formInput.employee,
+      formInput.workingHours,
+      '',
+      '',
+      false
+    );
+    workingHour.orderId = orderId;
+    this.addWorkingHourToFirebaseWorkingHourTable(workingHour);
+  }
+
+  public addWorkingHourToFirebaseWorkingHourTable(woringHour: any): void {
+    if (this.firestoreWorkingHourService !== undefined) {
+      // check if working hour is already in firestore
+      this.firestoreWorkingHourService
+        .checkIfWorkingHourExistsInOrderInFirestore(woringHour)
+        .then((isAlreadyInFirestore: boolean) => {
+          if (!isAlreadyInFirestore) {
+            this.firestoreWorkingHourService
+              .addWorkingHour(woringHour)
+              .then((id: string) => {
+                this.messageService.workingHourCreatedSuccessfully();
+                this.router.navigate(['order-details', woringHour.orderId]);
+                woringHour.id = id;
+              })
+              .catch((e) => {
+                console.error('can´t create working hour to firebase', e);
+              });
+          } else {
+            this.messageService.workingHourAlreadyExists();
+          }
+        });
+    }
+  }
+
+  get getFormControl() {
+    return this.createWorkingHourForm.controls;
+  }
+
+  public onSubmit() {
+    this.submitted = true;
+    if (this.createWorkingHourForm.invalid) {
+      return;
+    } else {
+      this.createWorkingHour(
+        this.createWorkingHourForm.value,
+        this.routeParamOrderId
+      );
+    }
+  }
+}

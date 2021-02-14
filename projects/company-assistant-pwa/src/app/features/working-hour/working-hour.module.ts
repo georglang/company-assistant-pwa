7c70d3421d20ab93f50ed90142bbbc66b075195e
Georg Lang <mail@georglang.com>
2021-02-14 12:11:30 +0100

    feat(working-hour): module add dependencies

--- projects/company-assistant-pwa/src/app/features/working-hour/working-hour.module.ts
+++ projects/company-assistant-pwa/src/app/features/working-hour/working-hour.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkingHourRoutingModule } from './working-hour-routing.module';
-import { WorkingHourListComponent } from './services/firestore-working-hour-service/working-hour-list/working-hour-list.component';
+import { WorkingHourListComponent } from './working-hour-list/working-hour-list.component';
+import { AngularMaterialModule } from '../../shared/material/angular-material.module';
+import { CreateWorkingHourComponent } from './create-working-hour/create-working-hour.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, WorkingHourRoutingModule],
-  declarations: [WorkingHourListComponent]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    WorkingHourRoutingModule
+  ],
+  declarations: [WorkingHourListComponent, CreateWorkingHourComponent]
 })
 export class WorkingHourModule {}

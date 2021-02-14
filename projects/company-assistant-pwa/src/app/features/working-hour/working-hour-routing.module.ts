f94b6185ef4c842fa810079f814ad47146ff71c5
Georg Lang <mail@georglang.com>
2021-02-14 13:38:43 +0100

    feat(working-hour): routes id and edit

--- projects/company-assistant-pwa/src/app/features/working-hour/working-hour-routing.module.ts
+++ projects/company-assistant-pwa/src/app/features/working-hour/working-hour-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WorkingHourListComponent } from './working-hour-list/working-hour-list.component';
 import { CreateWorkingHourComponent } from './create-working-hour/create-working-hour.component';
+import { EditWorkingHourComponent } from './edit-working-hour/edit-working-hour.component';
 
 WorkingHourListComponent;
 const routes: Routes = [
@@ -13,7 +14,17 @@ const routes: Routes = [
   {
     path: 'create-working-hour',
     component: CreateWorkingHourComponent
-  }
+  },
+  {
+    path: ':id',
+    children: [
+      {
+        path: 'edit',
+        component: EditWorkingHourComponent
+      }
+
+    ]
+  },
 ];
 
 @NgModule({

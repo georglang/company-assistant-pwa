5a851c0454f027559d90f5c40931f0bc28c435e3
Georg Lang <mail@georglang.com>
2021-02-14 13:39:12 +0100

    feat(config): employees

---
+++ projects/company-assistant-pwa/src/app/shared/config-data/employees.ts
@@ -0,0 +1,26 @@
+export const employees = [
+  {
+    value: 'Erle Wastl',
+    viewValue: 'Erle Wastl'
+  },
+  {
+    value: 'Greisl Martin',
+    viewValue: 'Greisl Martin'
+  },
+  {
+    value: 'Martin Johann',
+    viewValue: 'Martin Johann'
+  },
+  {
+    value: 'Speer Leonhard',
+    viewValue: 'Speer Leonhard'
+  },
+  {
+    value: 'Strobel Michael',
+    viewValue: 'Strobel Michael'
+  },
+  {
+    value: 'Tschabi Matthias',
+    viewValue: 'Tschabi Matthias'
+  }
+];

b5c43f7a2e6e342cc0b9f16faf030b69c9b55d2d
Georg Lang <mail@georglang.com>
2021-02-14 07:38:18 +0100

    feat(lazy-loaded-tab-nav): tab data

---
+++ projects/company-assistant-pwa/src/app/features/order/lazy-loaded-tab-navigation/TabData.ts
@@ -0,0 +1,22 @@
+import { ITabItem } from './ITabItem';
+
+export const tabs: ITabItem[] = [
+  {
+    feature: 'workingHours',
+    label: 'Stunden',
+    icon: 'access_time',
+    route: '',
+  },
+  {
+    feature: 'materials',
+    label: 'Material',
+    icon: 'material',
+    route: '',
+  },
+  {
+    feature: 'notes',
+    label: 'Notizen',
+    icon: 'sticky_note_2',
+    route: '',
+  },
+];

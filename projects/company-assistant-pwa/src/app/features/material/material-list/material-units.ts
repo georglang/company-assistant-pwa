2b4ff9d5e6dc40956e226bb522ebea6a71870ea4
Georg Lang <mail@georglang.com>
2021-02-20 20:16:35 +0100

    feat(material): material units

---
+++ projects/company-assistant-pwa/src/app/features/material/material-list/material-units.ts
@@ -0,0 +1,8 @@
+import { IUnit } from './IUnit';
+
+export const materialUnits: IUnit[] = [
+  { value: 'unit-0', viewValue: 'kg' },
+  { value: 'unit-0', viewValue: 'Meter' },
+  { value: 'unit-0', viewValue: 'qm' },
+  { value: 'unit-0', viewValue: 'Stück' }
+];

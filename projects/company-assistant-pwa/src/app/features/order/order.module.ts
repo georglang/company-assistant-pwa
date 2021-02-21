47bc3478f74d4e109e70cfdf8ffc7729616ad86e
Georg Lang <mail@georglang.com>
2021-02-21 19:05:51 +0100

    fix(order): import routing import path

--- projects/company-assistant-pwa/src/app/features/order/order.module.ts
+++ projects/company-assistant-pwa/src/app/features/order/order.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OrderRoutingModule } from './order.routing';
+import { OrderRoutingModule } from './order-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { OrderListComponent } from './order-list/order-list.component';
 import { EditOrderComponent } from './edit-order/edit-order.component';

af5ff0cbf36c76e02450434a5b5a923690a8d0ff
Georg Lang <mail@georglang.com>
2021-03-11 05:29:42 +0100

    feat(material): add edit component and shared module

--- projects/company-assistant-pwa/src/app/features/material/material.module.ts
+++ projects/company-assistant-pwa/src/app/features/material/material.module.ts
@@ -5,13 +5,20 @@ import { AngularMaterialModule } from '../../shared/material/angular-material.mo
 import { MaterialRoutingModule } from './material-routing.module';
 import { CreateMaterialComponent } from './create-material/create-material.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EditMaterialComponent } from './edit-material/edit-material.component';
+import { SharedModule } from '../../shared/shared.module';
 @NgModule({
   imports: [
     CommonModule,
     AngularMaterialModule,
     MaterialRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
-  declarations: [MaterialListComponent, CreateMaterialComponent]
+  declarations: [
+    MaterialListComponent,
+    CreateMaterialComponent,
+    EditMaterialComponent
+  ]
 })
 export class MaterialModule {}

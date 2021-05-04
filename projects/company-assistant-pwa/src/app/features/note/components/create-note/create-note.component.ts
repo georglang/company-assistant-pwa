f48aa27e343419c3cb9c86bd6ccd4bde885320c9
= <=>
2021-05-04 13:37:28 +0200

    feat(create-note): upload image to firestore and save url in notes collection

--- projects/company-assistant-pwa/src/app/features/note/components/create-note/create-note.component.ts
+++ projects/company-assistant-pwa/src/app/features/note/components/create-note/create-note.component.ts
@@ -4,6 +4,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { IOrder } from '../../../order/Order';
 import { FirestoreOrderService } from '../../../order/services/firestore-order-service/firestore-order.service';
 import { Location } from '@angular/common';
+import { INote } from '../../INote';
+import { Note } from '../../Note';
+import { ImageService } from '../../../../shared/services/image-service/image.service';
+import { FirestoreNoteService } from '../../services/firestore-note/firestore-note.service';
+import { MessageService } from '../../../../shared/services/message-service/message.service';
 
 @Component({
   selector: 'app-create-note',
@@ -14,18 +19,22 @@ export class CreateNoteComponent implements OnInit {
   createNoteFormGroup: FormGroup;
   order: IOrder;
   paramOrderId;
+  private imageEvent;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
     private location: Location,
-    private firestoreOrderService: FirestoreOrderService
+    private firestoreOrderService: FirestoreOrderService,
+    private firestoreNoteService: FirestoreNoteService,
+    private messageService: MessageService,
+    private imageService: ImageService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.createNoteFormGroup = this.fb.group({
-      note: ['', Validators.required]
+      notice: ['', Validators.required]
     });
 
     this.route.params.subscribe((params) => {
@@ -34,15 +43,55 @@ export class CreateNoteComponent implements OnInit {
     });
   }
 
-  public navigateToOrderList(): void {
+  fileInputOnChange($event): void {
+    this.imageEvent = $event;
+  }
+
+  navigateToOrderList(): void {
     this.location.back();
   }
 
-  private getOrderByIdFromCloudDatabase(orderId: string) {
+  saveNote(): void {
+    if (this.createNoteFormGroup.invalid) {
+      return;
+    } else {
+      this.createNote(
+        this.createNoteFormGroup.controls.notice.value,
+        this.paramOrderId
+      );
+    }
+  }
+
+  private getOrderByIdFromCloudDatabase(orderId: string): void {
     this.firestoreOrderService.getOrderById(orderId).then((order: IOrder) => {
       if (order !== undefined) {
         this.order = order;
       }
     });
+  }
+
+  private createNote(notice: string, orderId: string): void {
+    if (this.imageService !== undefined && this.imageEvent !== undefined) {
+      this.imageService
+        .upload(this.imageEvent)
+        .subscribe((imageUrl: string) => {
+          const note = new Note(notice, imageUrl, orderId);
+          this.addNoteToFirestoreNotesTable(note);
+        });
+    }
+  }
+
+  private addNoteToFirestoreNotesTable(note: INote): void {
+    if (this.firestoreNoteService !== undefined) {
+      this.firestoreNoteService
+        .addNote(note)
+        .then((id: string) => {
+          this.messageService.noteCreatedSuccessfully();
+          note.id = id;
+        })
+        .catch((e) => {
+          console.error('Can´t add note to firebase', e);
+        });
+    }
   }
 }

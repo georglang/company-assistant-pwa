66197999df67698898c6f0111aa1bd2e6d1e38e6
= <=>
2021-05-04 15:51:51 +0200

    feat(note): firestore-note.service

---
+++ projects/company-assistant-pwa/src/app/features/note/services/firestore-note/firestore-note.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import {
+  AngularFirestoreCollection,
+  AngularFirestore
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { INote } from '../../INote';
+import { IOrder } from '../../../order/Order';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreNoteService {
+  private ordersCollection: AngularFirestoreCollection<IOrder>;
+  private notesCollection: AngularFirestoreCollection<INote>;
+
+  constructor(private firestoreDb: AngularFirestore) {
+    this.ordersCollection = this.firestoreDb.collection<IOrder>('orders');
+    this.notesCollection = this.firestoreDb.collection<INote>('notes');
+  }
+
+  addNote(note: INote) {
+    const _note = { ...note };
+    debugger;
+    return this.ordersCollection
+      .doc(_note.orderId)
+      .collection('notes')
+      .add(_note)
+      .then((docReference) => {
+        return docReference.id;
+      })
+      .catch((error) => {
+        console.error('Error adding note: ', error);
+      });
+  }
+
+  getAllNotesById(orderId: string): Observable<INote[]> {
+    return this.ordersCollection
+      .doc(orderId)
+      .collection('notes')
+      .snapshotChanges()
+      .pipe(map((actions) => actions.map(this.documentToDomainObject)));
+  }
+
+  getMaterialByOrderId(orderId: string): any {
+    return this.ordersCollection
+      .doc(orderId)
+      .collection('notes')
+      .snapshotChanges()
+      .pipe(
+        map((actions) =>
+          actions.map((a) => {
+            const data = a.payload.doc.data() as any;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })
+        )
+      );
+  }
+
+  private documentToDomainObject = (dToDO) => {
+    const object = dToDO.payload.doc.data();
+    object.id = dToDO.payload.doc.id;
+    return object;
+  };
+}

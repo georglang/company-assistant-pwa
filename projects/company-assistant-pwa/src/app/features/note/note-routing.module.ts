b0f7bf6f4649cbe17a8b33b58c7d6c1af01657e0
= <=>
2021-05-11 16:26:51 +0200

    feat(note): add routes and components

--- projects/company-assistant-pwa/src/app/features/note/note-routing.module.ts
+++ projects/company-assistant-pwa/src/app/features/note/note-routing.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NotesListComponent } from './notes-list/notes-list.component';
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { EditNoteComponent } from './components/edit-note/edit-note.component';
 const routes: Routes = [
   {
     path: '',
-    component: NotesListComponent,
+    component: NotesListComponent
   },
+  {
+    path: 'create',
+    component: CreateNoteComponent
+  },
+  {
+    path: ':id',
+    children: [
+      {
+        path: 'edit',
+        component: EditNoteComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class NoteRoutingModule {}
--- projects/company-assistant-pwa/src/app/features/note/note.module.ts
+++ projects/company-assistant-pwa/src/app/features/note/note.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoteRoutingModule } from './note-routing.module';
-import { NotesListComponent } from './notes-list/notes-list.component';
 import { AngularMaterialModule } from '../../shared/material/angular-material.module';
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { EditNoteComponent } from './components/edit-note/edit-note.component';
+import { CrystalLightboxModule } from '@crystalui/angular-lightbox';
 
 @NgModule({
-  imports: [CommonModule, AngularMaterialModule, NoteRoutingModule],
-  declarations: [NotesListComponent]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    NoteRoutingModule,
+    CrystalLightboxModule
+  ],
+  declarations: [NotesListComponent, EditNoteComponent, CreateNoteComponent]
 })
 export class NoteModule {}

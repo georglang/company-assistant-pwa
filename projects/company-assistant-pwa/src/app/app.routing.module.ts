a43011c4e45b9672324fda5bb6b8bb058b0e7dee
Georg Lang <georglang0788@gmx.net>
2021-05-11 17:14:10 +0200

    feat(app-routing): include note module and impressum component

--- projects/company-assistant-pwa/src/app/app.routing.module.ts
+++ projects/company-assistant-pwa/src/app/app.routing.module.ts
@@ -4,17 +4,28 @@ import { HomeComponent } from './features/home/home.component';
 import { SignUpComponent } from './core/auth/sign-up/sign-up.component';
 import { SignInComponent } from './core/auth/sign-in/sign-in.component';
 import { ForgotpasswordComponent } from './core/auth/forgot-password/forgot-password.component';
+import { ImpressumComponent } from './shared/components/impressum/impressum.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  // { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'password-reset', component: ForgotpasswordComponent },
-  { path: 'home', component: HomeComponent },
+  // { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'orders', pathMatch: 'full' },
   {
     path: 'orders',
     loadChildren: () =>
       import('./features/order/order.module').then((m) => m.OrderModule)
+  },
+  {
+    path: 'notes',
+    loadChildren: () =>
+      import('./features/note/note.module').then((m) => m.NoteModule)
+  },
+  {
+    path: 'impressum',
+    component: ImpressumComponent
   }
 ];
 

40a3116ccf6ef60a08f716fa97cd94cd28dcf323
Georg Lang <georglang0788@gmx.net>
2021-06-21 17:45:16 +0200

    feat(user): extend user class

--- projects/company-assistant-pwa/src/app/core/auth/user.ts
+++ projects/company-assistant-pwa/src/app/core/auth/user.ts
@@ -1,6 +1,34 @@
+import { IRoles } from './roles';
 export interface IUser {
-  uid: string;
-  email: string;
   displayName: string;
+  email: string;
+  emailVerified?: boolean;
+  photoUrl?: string;
+  roles: IRoles;
+  uid: string;
+}
+
+export class User implements IUser {
+  displayName: string;
+  email: string;
   emailVerified: boolean;
+  photoUrl: string;
+  roles: IRoles;
+  uid: string;
+
+  constructor(
+    displayName: string,
+    email: string,
+    emailVerified: boolean,
+    photoUrl: string,
+    roles: IRoles,
+    uid: string
+  ) {
+    this.displayName = displayName;
+    this.email = email;
+    this.emailVerified = emailVerified;
+    this.photoUrl = photoUrl;
+    this.roles = roles;
+    this.uid = uid;
+  }
 }

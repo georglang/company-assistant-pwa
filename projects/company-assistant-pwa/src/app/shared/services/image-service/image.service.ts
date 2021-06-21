1de0a557e1b1b8c5782d40ae1f3e2f0e5ae17863
Georg Lang <georglang0788@gmx.net>
2021-06-21 17:57:30 +0200

    feat(image.service)

--- projects/company-assistant-pwa/src/app/shared/services/image-service/image.service.ts
+++ projects/company-assistant-pwa/src/app/shared/services/image-service/image.service.ts
@@ -5,8 +5,8 @@ import {
   AngularFireUploadTask
 } from '@angular/fire/storage';
 
-import { finalize, last, map, mergeMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { last, mergeMap } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -23,17 +23,25 @@ export class ImageService {
       event.target.files[0].name
     }`;
     const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, event.target.files[0]);
 
-    return this.storage
-      .upload(filePath, event.target.files[0])
-      .snapshotChanges()
-      .pipe(
-        last(),
-        mergeMap(() => {
-          const url = fileRef.getDownloadURL();
-          console.log('download url is ', url);
-          return url;
-        })
-      );
+    task.percentageChanges().subscribe((percentage) => {
+      this.changeImageUploadProgress(percentage);
+    });
+
+    return task.snapshotChanges().pipe(
+      last(),
+      mergeMap(() => {
+        const url = fileRef.getDownloadURL();
+        return url;
+      })
+    );
   }
+
+  private changeImageUploadProgress(percentage: number): void {
+    this.uploadProgressSource.next(percentage);
+  }
+
+  private uploadProgressSource = new BehaviorSubject<number>(0);
+  changePercentage$ = this.uploadProgressSource.asObservable();
 }

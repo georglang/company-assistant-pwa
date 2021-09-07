fdca9a4468e2bc8d168750521bc936a3aeff5ee2
Georg Lang <georglang0788@gmx.net>
2021-09-07 16:42:46 +0200

    feat(app.routing): uncomment currently unused routes

--- projects/company-assistant-pwa/src/app/app.routing.module.ts
+++ projects/company-assistant-pwa/src/app/app.routing.module.ts
@@ -1,32 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { HomeComponent } from './features/home/home.component';
 import { SignUpComponent } from './core/auth/sign-up/sign-up.component';
 import { SignInComponent } from './core/auth/sign-in/sign-in.component';
 import { ForgotpasswordComponent } from './core/auth/forgot-password/forgot-password.component';
 import { ImpressumComponent } from './shared/components/impressum/impressum.component';
+import { AdminGuard } from './core/auth/guards/admin.guard';
+import { CanReadGuard } from './core/auth/guards/can-read.guard';
+import { AdminComponent } from './features/admin/admin.component';
+import {
+  AngularFireAuthGuard,
+  canActivate,
+  redirectUnauthorizedTo,
+  redirectLoggedInTo
+} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['sign-in']);
+const redirectLoggedInToOrders = () => redirectLoggedInTo(['orders']);
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent },
-  { path: 'password-reset', component: ForgotpasswordComponent },
-  // { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'orders', pathMatch: 'full' },
+  { path: '', redirectTo: '/orders', pathMatch: 'full' },
+  // { path: 'admin', component: AdminComponent}
   {
     path: 'orders',
     loadChildren: () =>
-      import('./features/order/order.module').then((m) => m.OrderModule)
+      import('./features/order/order.module').then((m) => m.OrderModule),
+    canActivate: [CanReadGuard]
+    // canActivate: [AngularFireAuthGuard],
+    // data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
-  {
-    path: 'notes',
-    loadChildren: () =>
-      import('./features/note/note.module').then((m) => m.NoteModule)
-  },
-  {
-    path: 'impressum',
-    component: ImpressumComponent
-  }
+  // {
+  //   path: 'admin',
+  //   loadChildren: () =>
+  //     import('./features/admin/admin.module').then((m) => m.AdminModule),
+  // }
+  // {
+  //   path: 'notes',
+  //   loadChildren: () =>
+  //     import('./features/note/note.module').then((m) => m.NoteModule)
+  // },
+  // {
+  //   path: 'impressum',
+  //   component: ImpressumComponent
+  // },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent }
+  // { path: 'password-reset', component: ForgotpasswordComponent }
 ];
 
 @NgModule({

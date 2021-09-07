f3d8a6eab1564ba65d7f7a3a9994c4d5dbf92afc
Georg Lang <georglang0788@gmx.net>
2021-09-07 16:43:57 +0200

    feat(order): add default values for workingHour, materials and notes

--- projects/company-assistant-pwa/src/app/features/order/Order.ts
+++ projects/company-assistant-pwa/src/app/features/order/Order.ts
@@ -1,5 +1,8 @@
 import { Timestamp } from '@firebase/firestore-types';
 import { IWorkingHour } from '../working-hour/IWorkingHour';
+import { INote } from '../note/INote';
+import { IMaterial } from '../material/material-list/IMaterial';
+import { materials } from '../material/material-list/materials';
 
 export interface IOrder {
   companyName: string;
@@ -8,6 +11,7 @@ export interface IOrder {
   location: string;
   id?: any;
   workingHours?: IWorkingHour[];
+  notes?: INote[];
 }
 
 export interface IFlattenOrder {
@@ -21,16 +25,20 @@ export class Order implements IOrder {
   public contactPerson: string;
   public date: Timestamp;
   public location: string;
-  public id?: any;
+  public id?: string;
   public workingHours?: IWorkingHour[];
+  public materials?: IMaterial[];
+  public notes?: INote[];
 
   constructor(
     date: Timestamp,
     companyName: string,
     contactPerson: string,
     location: string,
-    id?: any,
-    workingHours?: IWorkingHour[]
+    id?: string,
+    workingHours: IWorkingHour[] = [],
+    materials: IMaterial[] = [],
+    notes: INote[] = []
   ) {
     this.companyName = companyName;
     this.contactPerson = contactPerson;
@@ -38,5 +46,7 @@ export class Order implements IOrder {
     this.id = id;
     this.location = location;
     this.workingHours = workingHours;
+    this.materials = materials;
+    this.notes = notes;
   }
 }

aa2a3daaaffe59084a6b020c63e3a297ba40fdc8
Georg Lang <georglang0788@gmx.net>
2021-10-07 21:40:33 +0200

    feat(app): add sub nav to shared module

--- projects/company-assistant-pwa/src/app/shared/shared.module.ts
+++ projects/company-assistant-pwa/src/app/shared/shared.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from './material/angular-material.module';
 import { FilterPipe } from './pipes/filter.pipe';
 import { WorkingHoursPipe } from './pipes/working-hours.pipe';
-
+import { SubNavigationComponent } from '../app/sub-navigation/sub-navigation.component';
 @NgModule({
   imports: [CommonModule, AngularMaterialModule],
-  declarations: [FilterPipe, WorkingHoursPipe],
-  exports: [AngularMaterialModule, FilterPipe, WorkingHoursPipe]
+  declarations: [FilterPipe, WorkingHoursPipe, SubNavigationComponent],
+  exports: [
+    AngularMaterialModule,
+    FilterPipe,
+    WorkingHoursPipe,
+    SubNavigationComponent
+  ]
 })
 export class SharedModule {}

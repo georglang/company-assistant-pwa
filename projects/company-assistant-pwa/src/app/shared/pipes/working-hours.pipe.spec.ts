203fb8b3ee576b557fd9fe556ef11d184d7f28eb
Georg Lang <georglang0788@gmx.net>
2021-10-11 19:45:35 +0200

    feat(working-hours): filter pipe

---
+++ projects/company-assistant-pwa/src/app/shared/pipes/working-hours.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { WorkingHoursPipe } from './working-hours.pipe';
+
+describe('Pipe: WorkingHourse', () => {
+  it('create an instance', () => {
+    let pipe = new WorkingHoursPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ projects/company-assistant-pwa/src/app/shared/pipes/working-hours.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IWorkingHour } from '../../features/working-hour/IWorkingHour';
+
+@Pipe({
+  name: 'workingHoursPipe'
+})
+export class WorkingHoursPipe implements PipeTransform {
+  transform(items: IWorkingHour[], searchText: string): IWorkingHour[] {
+    if (!items) return [];
+    if (!searchText) return items;
+    searchText = searchText.toLowerCase();
+    return items.filter((item) => {
+      return (
+        item.description.toLowerCase().includes(searchText) ||
+        item.employee.toLowerCase().includes(searchText)
+      );
+    });
+  }
+}

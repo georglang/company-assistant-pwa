7b4425a2af83ff9fabedbb7c0a020a5750670f0f
Georg Lang <georglang0788@gmx.net>
2021-10-22 17:58:48 +0200

    feat(edit-note): image upload with preview

--- projects/company-assistant-pwa/src/app/features/note/components/edit-note/edit-note.component.ts
+++ projects/company-assistant-pwa/src/app/features/note/components/edit-note/edit-note.component.ts
@@ -1,41 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { FirestoreNoteService } from '../../services/firestore-note/firestore-note.service';
-import { Note } from '../../Note';
 import { INote } from '../../INote';
-import { MessageService } from '../../../../shared/services/message-service/message.service';
 import { ToastrService } from 'ngx-toastr';
 import { ImageService } from 'projects/company-assistant-pwa/src/app/shared/services/image-service/image.service';
+import { Subscription, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-note',
   templateUrl: './edit-note.component.html',
   styleUrls: ['./edit-note.component.scss']
 })
-export class EditNoteComponent implements OnInit {
+export class EditNoteComponent implements OnInit, OnDestroy {
   editNoteForm: FormGroup;
   percentageImageUpload;
-  fileName = '';
   imageEvent;
   subNavTitle = 'Notiz bearbeiten';
   enableSubNavBackBtn = true;
-  noteId: string;
   note: INote;
   submitted = false;
   filePath: string;
-
   currentlySelectedImageUrl: string;
+  orderId: string;
 
-  private orderId: string;
+  private subscriptions = new Subscription();
 
   constructor(
     private fb: FormBuilder,
     private location: Location,
     private route: ActivatedRoute,
     private firestoreNoteService: FirestoreNoteService,
-    private messageService: MessageService,
     private toastrService: ToastrService,
     private imageService: ImageService
   ) {}
@@ -43,44 +40,29 @@ export class EditNoteComponent implements OnInit {
   ngOnInit(): void {
     this.editNoteForm = this.fb.group({
       notice: ['', Validators.required],
-      img: [null],
-      filename: ['']
+      img: [null]
     });
 
     this.route.parent.parent.params.subscribe((url) => {
-      this.orderId = url.id;
       this.route.parent.params.subscribe((subUrl) => {
-        this.noteId = subUrl.id;
         this.getNoteByIdFromFirebase(url.id, subUrl.id);
       });
     });
   }
 
-  fileInputOnChange($event): void {
-    this.fileName = $event.currentTarget.files[0].name;
-    this.imageEvent = $event;
-  }
-
-  imagePreview($event) {
+  imagePreview($event): void {
     const file = ($event.target as HTMLInputElement).files[0];
-
     this.editNoteForm.patchValue({
       img: file
     });
-
     this.editNoteForm.get('img').updateValueAndValidity();
 
     const reader = new FileReader();
     reader.onload = () => {
-      this.filePath = reader.result as string;
+      this.note.imageUrl = reader.result as string;
     };
     reader.readAsDataURL(file);
     this.imageEvent = $event;
-    debugger;
-  }
-
-  submit() {
-    console.log(this.editNoteForm.value);
   }
 
   get getEditNoteFormControls() {
@@ -91,82 +73,37 @@ export class EditNoteComponent implements OnInit {
     this.location.back();
   }
 
-  // saveNote(): void {
-  //   const note = new Note(this.editNoteForm.controls.notice.value);
-
-  //   this.submitted = true;
-  //   if (this.editNoteForm.invalid) {
-  //     return;
-  //   } else {
-  //     this.checkIfNoteExistsInOrderInFirestore(note);
-  //   }
-  // }
-
   saveNote(): void {
-    const note = new Note(
-      this.orderId,
-      this.editNoteForm.value.notice,
-      this.editNoteForm.value.img
-    );
-    debugger;
+    const note = this.note;
+    note.notice = this.editNoteForm.value.notice;
 
-    this.createNote(this.editNoteForm.value.notice, this.orderId);
-
-    // this.submitted = true;
-    // if (this.editNoteForm.invalid) {
-    //   return;
-    // } else {
-    //   this.checkIfNoteExistsInOrderInFirestore(note);
-    // }
+    if (this.imageEvent) {
+      this.uploadImage().subscribe((newImageUrl: string) => {
+        note.imageUrl = newImageUrl;
+        this.updateNoteToFirestoreNotesTable(note);
+      });
+    } else {
+      this.updateNoteToFirestoreNotesTable(note);
+    }
   }
 
-  private createNote(notice: string, orderId: string): void {
+  private uploadImage(): Observable<string> {
     if (this.imageService !== undefined && this.imageEvent !== undefined) {
-      this.imageService
-        .upload(this.imageEvent)
-        .subscribe((imageUrl: string) => {
-          const note = new Note(orderId, notice, imageUrl);
-          this.updateNoteToFirestoreNotesTable(note);
-        });
-    } else {
-      this.updateNoteToFirestoreNotesTable(new Note(orderId, notice, ''));
+      return this.imageService.upload(this.imageEvent);
     }
   }
 
   private updateNoteToFirestoreNotesTable(note: INote): void {
-    if (this.firestoreNoteService !== undefined) {
+    if (this.firestoreNoteService) {
       this.firestoreNoteService
-        .updateNote(this.orderId, note)
-        .then((id: any) => {
-          this.messageService.noteCreatedSuccessfully();
-          note.id = id;
+        .updateNote(note)
+        .then(() => {
+          this.showUpdateMessage();
           this.navigateToNotesList();
         })
         .catch((e) => {
-          console.error('Can´t add note to firebase', e);
+          console.error('Can´t update note in firebase', e);
         });
-    }
-  }
-
-  private checkIfNoteExistsInOrderInFirestore(note: INote) {
-    this.firestoreNoteService
-      .checkIfNoteExistsInOrderInFirestore(note)
-      .then((doesNoteExist) => {
-        if (!doesNoteExist) {
-          this.updateNoteInFirestore(this.orderId, note);
-        } else {
-          this.messageService.materialAlreadyExists();
-        }
-      });
-  }
-
-  private updateNoteInFirestore(orderId: string, note: INote): void {
-    if (this.firestoreNoteService !== undefined) {
-      this.setControl(note);
-      this.firestoreNoteService.updateNote(orderId, note).then(() => {
-        this.showUpdateMessage();
-        this.navigateToNotesList();
-      });
     }
   }
 
@@ -187,24 +124,27 @@ export class EditNoteComponent implements OnInit {
   }
 
   private getNoteByIdFromFirebase(orderId: string, noteId: string) {
-    const getNoteByOrderId = this.firestoreNoteService
+    const getNotes$ = this.firestoreNoteService
       .getNotesByOrderId(orderId)
-      .subscribe((notes: any) => {
-        const note = notes.forEach((note) => {
-          debugger;
-          this.note = new Note(
-            (note.orderId = note.orderId),
-            note.notice,
-            note.imageUrl
-          );
-          this.setControl(note);
-        });
+      .pipe(map((notes) => notes.filter((note) => note.id === noteId)))
+      .subscribe((note: INote[]) => {
+        this.note = note[0];
+        this.setControl(note[0]);
       });
+    this.subscriptions.add(getNotes$);
   }
 
-  setControl(note: INote): void {
+  private setControl(note: INote): void {
     this.editNoteForm.setValue({
-      notice: note.notice
+      notice: note.notice,
+      img: this.imageEvent ?? []
     });
+    this.note.imageUrl = note.imageUrl;
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscriptions != undefined) {
+      this.subscriptions.unsubscribe();
+    }
   }
 }

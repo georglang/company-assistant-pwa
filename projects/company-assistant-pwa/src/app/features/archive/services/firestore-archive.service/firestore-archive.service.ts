88e9e7f4ff3708e3d4004e9dbb040b92ba7af098
Georg Lang <georglang0788@gmx.net>
2022-06-01 20:57:25 +0200

    feat(archive): remove combineLatest

--- projects/company-assistant-pwa/src/app/features/archive/services/firestore-archive.service/firestore-archive.service.ts
+++ projects/company-assistant-pwa/src/app/features/archive/services/firestore-archive.service/firestore-archive.service.ts
@@ -11,16 +11,15 @@ import { combineLatest, Observable, of } from 'rxjs';
 import { IWorkingHour } from '../../../working-hour/IWorkingHour';
 import { IMaterial } from '../../../material/material-list/IMaterial';
 import { INote } from '../../../note/INote';
+
 import { FirestoreWorkingHourService } from '../../../working-hour/services/firestore-working-hour-service/firestore-working-hour.service';
 import { FirestoreMaterialService } from '../../../material/services/firestore-material-service/firestore-material.service';
 import { FirestoreNoteService } from '../../../note/services/firestore-note/firestore-note.service';
-
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreArchiveService {
   private archiveCollection: AngularFirestoreCollection<IOrder>;
-
   private ordersCollection: AngularFirestoreCollection<IOrder>;
 
   constructor(
@@ -34,85 +33,54 @@ export class FirestoreArchiveService {
     this.ordersCollection = this.firestore.collection<IOrder>('orders');
   }
 
-  public getOrdersWithSubcollections(): Observable<any[]> {
-    return this.firestoreService.colWithIds$('orders').pipe(
-      mergeMap((orders: any[]) => {
-        const workingHours = orders.map((order: IOrder) => {
-          return this.firestoreService
-            .getCollection$(`orders/${order.id as string}/workingHours`)
-            .pipe(map((workingHours: IWorkingHour[]) => workingHours));
-        });
+  getOrdersWithSubcollections(): Promise<IOrder[]> {
+    const _orders: IOrder[] = [];
+    return new Promise((resolve) => {
+      this.ordersCollection
+        .snapshotChanges()
+        .pipe(map((actions) => actions.map(this.documentToDomainObject)))
+        .subscribe((orders: IOrder[]) => {
+          if (orders.length > 0) {
+            orders.map((order: IOrder) => {
+              const orders$ = () => {
+                return Promise.all([
+                  this.materialService
+                    .getMaterialsByOrderId(order.id)
+                    .pipe(take(1))
+                    .toPromise(),
+                  this.workingHourService
+                    .getWorkingHoursByOrderId(order.id)
+                    .pipe(take(1))
+                    .toPromise(),
+                  this.notesService
+                    .getNotesByOrderId(order.id)
+                    .pipe(take(1))
+                    .toPromise()
+                ]);
+              };
 
-        const materials = orders.map((order: IOrder) => {
-          return this.firestoreService
-            .getCollection$(`orders/${order.id as string}/materials`)
-            .pipe(map((materials: IMaterial[]) => materials));
-        });
+              void orders$().then((val) => {
+                order.materials = val[0];
+                order.workingHours = val[1];
+                order.notes = val[2];
+                _orders.push(order);
+                console.log('Promise.all Result:', order);
+                resolve(_orders);
+              });
+              console.log('11111');
 
-        const notes = orders.map((order: IOrder) => {
-          return this.firestoreService
-            .getCollection$(`orders/${order.id as string}/notes`)
-            .pipe(map((notes: INote[]) => notes));
-        });
-
-        return combineLatest([
-          of(orders),
-          ...workingHours,
-          ...materials,
-          ...notes
-        ]);
-      })
-      // map(([orders, workingHours, materials, notes]) => {
-      //   debugger;
-      //   return orders.map((order: IOrder) => {
-      //     order.workingHours = workingHours.find(
-      //       (workingHour: IWorkingHour) => workingHour.orderId === order.id
-      //     );
-
-      //     order.materials = materials.find(
-      //       (material: IMaterial) => material.orderId === order.id
-      //     );
-
-      //     order.notes = notes.find((note: INote) => note.orderId === order.id);
-      //     return order;
-      //   });
-      // })
-    );
-  }
-
-  getOrdersWithSubcollections2() {
-    this.ordersCollection
-      .snapshotChanges()
-      .pipe(map((actions) => actions.map(this.documentToDomainObject)))
-      .subscribe((orders: IOrder[]) => {
-        if (orders.length > 0) {
-          orders.map((order: IOrder) => {
-            const orders$ = () => {
-              return Promise.all([
-                this.materialService
-                  .getMaterialsByOrderId(order.id)
-                  .pipe(take(1))
-                  .toPromise(),
-                this.workingHourService
-                  .getWorkingHoursByOrderId(order.id)
-                  .pipe(take(1))
-                  .toPromise(),
-                this.notesService
-                  .getNotesByOrderId(order.id)
-                  .pipe(take(1))
-                  .toPromise()
-              ]);
-            };
-
-            void orders$().then((val) => {
-              order.materials = val[0];
-              order.workingHours = val[1];
-              order.notes = val[2];
-              console.log('Promise.all Result:', order);
+              resolve(_orders);
             });
-          });
-        }
-      });
+            console.log('22222');
+            resolve(_orders);
+          }
+          console.log('33333');
+          resolve(_orders);
+        });
+      console.log('4444');
+      resolve(_orders);
+    });
+    console.log('5555');
   }
 
   getArchivedData(): Observable<IOrder[]> {

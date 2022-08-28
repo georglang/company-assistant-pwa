822432f3d0e1d8ec7bdee5e865e742cb60dff723
Georg Lang <georglang0788@gmx.net>
2022-08-28 09:24:07 +0200

    feat(archive): list archived orders

--- projects/company-assistant-pwa/src/app/features/archive/archive-order-list.component.ts
+++ projects/company-assistant-pwa/src/app/features/archive/archive-order-list.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+import { MatSelectionList } from '@angular/material/list';
+import { IOrder } from '../order/Order';
+import { FirestoreOrderService } from '../order/services/firestore-order-service/firestore-order.service';
+import { ConfirmDeleteDialogComponent } from '../../shared/components/confirm-delete-dialog/confirm-delete-dialog.component';
+import { MessageService } from '../../shared/services/message-service/message.service';
+import { SearchService } from '../../shared/services/search.service';
 import { FirestoreArchiveService } from './services/firestore-archive.service/firestore-archive.service';
-import { IOrder, Order } from '../order/Order';
-import { materials } from '../material/material-list/materials';
 
 @Component({
   selector: 'app-archive',
@@ -9,31 +17,158 @@ import { materials } from '../material/material-list/materials';
   styleUrls: ['./archive-order-list.component.scss']
 })
 export class ArchiveOrderlistComponent implements OnInit {
-  private archivedOrders: IOrder[];
-  constructor(private archiveService: FirestoreArchiveService) {}
+  // private archivedOrders: IOrder[];
+  // constructor(private archiveService: FirestoreArchiveService) {}
 
-  ngOnInit() {
-    this.archiveService.getOrdersWithSubcollections().subscribe((data) => {
-      const test = data[2].materials;
-      const test2 = data[1].workingHours;
+  // ngOnInit() {
+  //   this.archiveService.getOrdersWithSubcollections().subscribe((data) => {
+  //     const test = data[2].materials;
+  //     const test2 = data[1].workingHours;
+  //     const order = new Order(
+  //       data[0][0].data.companyName,
+  //       data[0][0].data.contactPerson,
+  //       data[0][0].data.date,
+  //       data[0][0].id,
+  //       data[0][0].data.location,
+  //       data[1].workingHours,
+  //       data[2].materials,
+  //       data[3].notes
+  //     );
+  //     debugger;
+  //     console.log('Order: ', order);
 
-      const order = new Order(
-        data[0][0].data.companyName,
-        data[0][0].data.contactPerson,
-        data[0][0].data.date,
-        data[0][0].id,
-        data[0][0].data.location,
-        data[1].workingHours,
-        data[2].materials,
-        data[3].notes
-      );
-      debugger;
+  //     // this.archivedOrders.push({
+  //     //   ...data[0],
+  //     //   ...data[1].workingHour,
+  //     //   ...
+  //     // });
+  //   });
+  // }
 
-      // this.archivedOrders.push({
-      //   ...data[0],
-      //   ...data[1].workingHour,
-      //   ...
-      // });
+  @ViewChild('searchbar') searchbar: ElementRef;
+  @ViewChild('orderList') orderList: MatSelectionList;
+
+  displayedColumns = ['date', 'customer', 'location'];
+  displayedColumnsWorkingHours = [
+    'date',
+    'workingHours',
+    'description',
+    'location'
+  ];
+  highlighted: SelectionModel<IOrder>;
+  selectedOrder: IOrder;
+  showButtonsIfOrderIsSelected = false;
+  showDeleteButton = true;
+  list: IOrder[] = [];
+  selectedOptions: IOrder[] = [];
+  searchText: string;
+  toggleSearch = false;
+  subNavTitle = 'Abgeschlossen';
+  enableSubNavBackBtn = false;
+  panelOpenState = false;
+  orders: IOrder[] = [];
+
+  constructor(
+    private router: Router,
+    public dialog: MatDialog,
+    private firestoreOrderService: FirestoreOrderService,
+    private messageService: MessageService,
+    private searchService: SearchService,
+    private archiveService: FirestoreArchiveService
+  ) {}
+
+  ngOnInit(): void {
+    this.highlighted = new SelectionModel<IOrder>(false, []);
+    // this.getOrders();
+
+    this.archiveService.getArchivedData().subscribe((orders: IOrder[]) => {
+      this.orders = orders;
+      console.log('Orders', this.orders);
     });
+
+    this.searchService.searchText$.subscribe((searchText: string) => {
+      this.searchText = searchText;
+    });
+  }
+
+  public deleteOrder(order: IOrder): void {
+    this.openDeleteOrderDialog(order.id);
+  }
+
+  private deleteOrderInFirebase(orderId: string) {
+    this.firestoreOrderService.deleteOrder(orderId).then((data) => {
+      this.showDeleteMessage();
+      this.getOrders();
+    });
+  }
+
+  public editOrder(order: IOrder): void {
+    this.router.navigate(['orders/' + order.id + '/edit-order']);
+  }
+
+  public getOrders(): void {
+    if (this.firestoreOrderService) {
+      this.firestoreOrderService
+        .getOrdersFromOrdersCollection()
+        .subscribe((orders: IOrder[]) => {
+          if (orders) {
+            const ordersSortedByDate = orders.sort(
+              (a, b) => b.date.seconds - a.date.seconds
+            );
+            this.list = ordersSortedByDate;
+          }
+        });
+    }
+  }
+
+  public navigateToOrder(order: IOrder): void {
+    this.router.navigate(['orders/' + order.id]);
+  }
+
+  public navigateToWorkingHours(order: IOrder): void {
+    this.router.navigate(['orders/' + order.id + '/working-hours']);
+  }
+
+  public navigateToCreateOrder(): void {
+    this.router.navigate(['orders/create-order']);
+  }
+
+  public openDeleteOrderDialog(orderId: string): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    const dialogRef = this.dialog.open(
+      ConfirmDeleteDialogComponent,
+      dialogConfig
+    );
+    dialogRef.afterClosed().subscribe((shouldDelete) => {
+      if (shouldDelete) {
+        this.deleteOrderInFirebase(orderId);
+      }
+    });
+  }
+
+  public showActionButtons(newSelectedOrder: IOrder): void {
+    if (this.selectedOrder === newSelectedOrder) {
+      this.orderList.deselectAll();
+      this.showButtonsIfOrderIsSelected = !this.showButtonsIfOrderIsSelected;
+    } else {
+      this.showButtonsIfOrderIsSelected = true;
+    }
+    this.selectedOrder = newSelectedOrder;
+  }
+
+  private showDeleteMessage() {
+    this.messageService.deletedSucessfull();
+  }
+
+  openSearch(): void {
+    this.toggleSearch = true;
+    this.searchbar.nativeElement.focus();
+  }
+  searchClose(): void {
+    this.searchService.changeSearchText('');
+    this.toggleSearch = false;
   }
 }
--- projects/company-assistant-pwa/src/app/features/archive/archive.module.ts
+++ projects/company-assistant-pwa/src/app/features/archive/archive.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArchiveOrderlistComponent } from './archive-order-list.component';
 import { ArchiveRoutingModule } from './archive.routing.module';
+import { SharedModule } from '../../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, ArchiveRoutingModule],
+  imports: [CommonModule, SharedModule, ArchiveRoutingModule, FormsModule],
   declarations: [ArchiveOrderlistComponent]
 })
 export class ArchiveModule {}

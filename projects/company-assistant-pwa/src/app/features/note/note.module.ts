4f9e43d4ec1e4bf6105d5a0e6f87ab781ab49640
Georg Lang <georglang0788@gmx.net>
2022-09-05 19:53:19 +0200

    core: remove CrystallLightboxModule

--- projects/company-assistant-pwa/src/app/features/note/note.module.ts
+++ projects/company-assistant-pwa/src/app/features/note/note.module.ts
@@ -6,7 +6,6 @@ import { CreateNoteComponent } from './components/create-note/create-note.compon
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NotesListComponent } from './components/notes-list/notes-list.component';
 import { EditNoteComponent } from './components/edit-note/edit-note.component';
-import { CrystalLightboxModule } from '@crystalui/angular-lightbox';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { SharedModule } from '../../shared/shared.module';
     ReactiveFormsModule,
     AngularMaterialModule,
     NoteRoutingModule,
-    CrystalLightboxModule,
     SharedModule,
     FormsModule
   ],

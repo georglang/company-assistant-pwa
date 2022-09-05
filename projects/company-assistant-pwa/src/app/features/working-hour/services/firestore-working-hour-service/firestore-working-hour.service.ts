c5c9c22470133270899174596b58f0d229882495
Georg Lang <georglang0788@gmx.net>
2022-09-05 19:50:01 +0200

    core: fix angular fire imports

--- projects/company-assistant-pwa/src/app/features/working-hour/services/firestore-working-hour-service/firestore-working-hour.service.ts
+++ projects/company-assistant-pwa/src/app/features/working-hour/services/firestore-working-hour-service/firestore-working-hour.service.ts
@@ -7,7 +7,7 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
   DocumentData
-} from '@angular/fire/firestore';
+} from '@angular/fire/compat/firestore';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
